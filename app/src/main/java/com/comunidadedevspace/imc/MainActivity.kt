c5e11fc27a9a489e7aea8dbbf157141f3f9a04ff
leeoobueno <leobuenovelho@gmail.com>
2024-09-13 12:25:32 +0100

    Home screen edittext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,29 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Recuperar os componentes EditText
+        // Criar uma variavel e associar o componente de UI <EditText>
+        // Recuperar o botao da tela
+        // Colocar a acao do botao
+        // Recuperar o texto digitado no edt peso
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.edt_weight)
+        val edtHeight = findViewById<TextInputEditText>(R.id.edt_height)
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+
+        btnCalculate.setOnClickListener {
+            val weight = edtWeight.text
+            val height = edtHeight.text
+
+            println("Leonardo " + height)
+        }
     }
 }
